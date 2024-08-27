fd4a2c5f327b54844fb8a608a76164c78a714479
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-08-27 00:42:56 -0300

    complete debugging and basic enumerables exercises

--- 9_debugging/exercises/debugging_exercises.rb
+++ 9_debugging/exercises/debugging_exercises.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 # First, we're going to practice reading the Stack Trace
 # Don't look at this method prior to running the test
 # Type 'rspec' into the terminal to run Rspec
@@ -10,7 +12,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -20,11 +22,11 @@ end
 # use the stack trace to debug the following method
 # Don't look at this method prior to running the test
 # Run rspec, let the test fail, and go through the stack trace again
-def increment_greatest_value(nested_array)
-  greatest_value = nested_array.flatten.min
-  nested_array.each do |array|
+def increment_greatest_value(nested_array) # [[3, 4], [1, 2]]
+  greatest_value = nested_array.flatten.min # 1
+  nested_array.each do |array| # [3, 4] and then [1, 2]
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value
         greatest_value = current_value
       end
     end
@@ -36,8 +38,10 @@ end
 # Use p and puts in order to find what's wrong with our method
 
 def isogram?(string)
-  original_length = string.length
-  string_array = string.downcase.split
+  p "STRING: #{string}" #Odin
+  original_length = string.length #4
+  string_array = string.downcase.split('') # <- should be .split('')?
+  p "ARRAY: #{string_array}"
   unique_length = string_array.uniq.length
   original_length == unique_length
 end
@@ -51,6 +55,6 @@ end
 
 def yell_greeting(string)
   name = string
-  name = name.downcase
+  name = name.upcase
   greeting = "WASSAP, #{name}!"
 end
